85fc326959849b272c5cf431151026db5b00b380
Connor Dillon <connor@cdillon.nl>
2021-08-04 11:38:26 +0200

    Add ThreadCount, spawn_logger and copy_output

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 #![feature(command_access)]
 
 use getopts::Options;
-use log::{error, warn};
-use serde::Deserialize;
+use log::{debug, error, info, trace, warn, Level};
+use serde::{Deserialize, Serialize};
 use serde_yaml::from_reader;
 use std::collections::hash_map::{DefaultHasher, RandomState};
 use std::collections::HashMap;
 use std::fs::{self, File};
 use std::hash::{BuildHasher, Hash, Hasher};
-use std::io::{self, Cursor, Read, Write};
+use std::io::{self, BufRead, BufReader, BufWriter, Cursor, Read, Write};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
+use std::sync::{Arc, Condvar, Mutex};
 use std::thread::JoinHandle;
 use std::{env, thread};
 use tar::{Archive, Entry};
@@ -25,6 +26,7 @@ fn main() {
     } else if let Some(cpath) = params.config {
         let cfile = File::open(cpath).unwrap();
         let conf: Config = from_reader(cfile).unwrap();
+        debug!("Config: {:?}", conf);
         if let Some(ipath) = params.input {
             let ifile = File::open(ipath).unwrap();
             let mut archive = Archive::new(ifile);
@@ -66,6 +68,7 @@ fn process_files<
     iter: T,
     output: V,
 ) -> io::Result<()> {
+    let tc = ThreadCount::new();
     let mut gen = Gen::new();
     let mut buf = Vec::with_capacity(1024);
     for entry_result in iter {
@@ -78,13 +81,15 @@ fn process_files<
                 Some(p) => {
                     let cur = Cursor::new(&mut buf);
                     let proc = prep_process(&mut gen, p);
-                    run_process(proc, cur.chain(entry), output(f)?)?;
+                    info!("Processing {} (type: {}) with {}", name, f, p.name);
+                    run_process(&tc, proc, cur.chain(entry), output(f)?)?;
                 }
                 None => error!("File type not included in config: {}", f),
             },
             None => warn!("File type not determined for {}", name),
         }
     }
+    tc.wait();
     Ok(())
 }
 
@@ -101,10 +106,35 @@ struct Plugin {
     stdout: Option<bool>,
 }
 
+#[derive(Clone)]
+struct ThreadCount(Arc<(Mutex<u32>, Condvar)>);
+
+impl ThreadCount {
+    fn new() -> ThreadCount {
+        ThreadCount(Arc::new((Mutex::new(0u32), Condvar::new())))
+    }
+
+    fn wait(&self) {
+        let (lock, cvar) = &*self.0;
+        let _guard = cvar.wait_while(lock.lock().unwrap(), |c| *c > 0).unwrap();
+    }
+
+    fn incr(&self) {
+        let mut count = self.0 .0.lock().unwrap();
+        *count = *count + 1;
+    }
+
+    fn decr(&self) {
+        let mut count = self.0 .0.lock().unwrap();
+        *count = *count - 1;
+        self.0 .1.notify_all();
+    }
+}
+
 fn get_file_type(_name: &str, head: &[u8]) -> Option<FileType> {
-    if &head[..4] == b"FILE" {
+    if head.len() >= 4 && &head[..4] == b"FILE" {
         Some("ntfs.mft".into())
-    } else if &head[..9] == b"#!/bin/sh" {
+    } else if head.len() >= 9 && &head[..9] == b"#!/bin/sh" {
         Some("script.sh".into())
     } else {
         None
@@ -125,29 +155,41 @@ impl<T: Read> Name for Entry<'_, T> {
 fn prep_process(gen: &mut Gen, plugin: &Plugin) -> PreppedProcess {
     let mut cmd = Command::new(&plugin.path);
     let mut args = plugin.args.clone().unwrap_or(Vec::new());
+    let cwd = env::current_dir().unwrap();
     let input_file_name = if plugin.stdin.unwrap_or(false) {
         cmd.stdin(Stdio::piped());
         None
     } else {
         cmd.stdin(Stdio::null());
-        let input_name = gen.gen_string();
-        cmd.env("INPUT", &input_name);
-        replace_arg(&mut args, "$INPUT", &input_name);
-        Some(input_name)
+        let mut input_path = cwd.clone();
+        input_path.push(gen.gen_string());
+        let input_path_str = String::from(input_path.to_str().unwrap());
+        cmd.env("INPUT", &input_path);
+        replace_arg(&mut args, "$INPUT", &input_path_str);
+        Some(input_path_str)
     };
     let output_file_name = if plugin.stdout.unwrap_or(false) {
         None
     } else {
-        let output_name = gen.gen_string();
-        cmd.env("OUTPUT", &output_name);
-        replace_arg(&mut args, "$OUTPUT", &output_name);
-        Some(output_name)
+        let mut output_cwd = cwd.clone();
+        let output_dir = String::from(output_cwd.to_str().unwrap());
+        replace_arg(&mut args, "$OUTPUT_DIR", &output_dir);
+        cmd.env("OUTPUT_DIR", &output_dir);
+        let output_file = gen.gen_string();
+        replace_arg(&mut args, "$OUTPUT_FILE", &output_file);
+        cmd.env("OUTPUT_FILE", &output_file);
+        output_cwd.push(output_file);
+        let output = String::from(output_cwd.to_str().unwrap());
+        cmd.env("OUTPUT", &output);
+        replace_arg(&mut args, "$OUTPUT", &output);
+        Some(output)
     };
     cmd.args(args).stdout(Stdio::piped()).stderr(Stdio::piped());
     PreppedProcess {
         command: cmd,
         input_file_name,
         output_file_name,
+        plugin_name: plugin.name.clone(),
     }
 }
 
@@ -155,6 +197,7 @@ struct PreppedProcess {
     command: Command,
     input_file_name: Option<String>,
     output_file_name: Option<String>,
+    plugin_name: String,
 }
 
 struct Gen(u64, String, DefaultHasher);
@@ -194,31 +237,33 @@ fn replace_arg(args: &mut Vec<String>, var: &str, rep: &str) {
 }
 
 fn run_process<T: Read, U: Write + Send + 'static>(
+    tc: &ThreadCount,
     mut proc: PreppedProcess,
     mut input: T,
-    mut output: U,
+    output: U,
 ) -> io::Result<()> {
+    let plugin_name = proc.plugin_name;
     match (proc.input_file_name, proc.output_file_name) {
         (Some(i), Some(o)) => {
-            let mut input_file = File::create(&i)?;
-            io::copy(&mut input, &mut input_file)?;
-            File::create(&o)?;
+            io::copy(&mut input, &mut File::create(&i)?)?;
             let mut child = proc.command.spawn()?;
-            spawn(move || {
+            spawn_logger(tc, Level::Info, child.stdout.take(), plugin_name.clone());
+            spawn_logger(tc, Level::Error, child.stderr.take(), plugin_name.clone());
+            spawn(tc.clone(), move || {
                 child.wait()?;
-                let mut output_file = File::open(&o)?;
-                io::copy(&mut output_file, &mut output)?;
+                let output_file = File::open(&o)?;
+                copy_output(plugin_name, output_file, output)?;
                 fs::remove_file(i)?;
                 fs::remove_file(o)
             });
         }
         (Some(i), None) => {
-            let mut input_file = File::create(&i)?;
-            io::copy(&mut input, &mut input_file)?;
+            io::copy(&mut input, &mut File::create(&i)?)?;
             let mut child = proc.command.spawn()?;
-            spawn(move || {
-                let mut stdout = child.stdout.take().unwrap();
-                io::copy(&mut stdout, &mut output)?;
+            spawn_logger(tc, Level::Error, child.stderr.take(), plugin_name.clone());
+            spawn(tc.clone(), move || {
+                let stdout = child.stdout.take().unwrap();
+                copy_output(plugin_name, stdout, output)?;
                 child.wait()?;
                 fs::remove_file(i)
             });
@@ -227,18 +272,21 @@ fn run_process<T: Read, U: Write + Send + 'static>(
             File::create(&o)?;
             let mut child = proc.command.spawn()?;
             let mut stdin = child.stdin.take().unwrap();
+            spawn_logger(tc, Level::Info, child.stdout.take(), plugin_name.clone());
+            spawn_logger(tc, Level::Error, child.stderr.take(), plugin_name.clone());
             io::copy(&mut input, &mut stdin)?;
-            spawn(move || {
+            spawn(tc.clone(), move || {
                 child.wait()?;
-                let mut output_file = File::open(&o)?;
-                io::copy(&mut output_file, &mut output)?;
+                let output_file = File::open(&o)?;
+                copy_output(plugin_name, output_file, output)?;
                 fs::remove_file(o)
             });
         }
         (None, None) => {
             let mut child = proc.command.spawn()?;
-            let mut stdout = child.stdout.take().unwrap();
-            spawn(move || io::copy(&mut stdout, &mut output));
+            spawn_logger(tc, Level::Error, child.stderr.take(), plugin_name.clone());
+            let stdout = child.stdout.take().unwrap();
+            spawn(tc.clone(), move || copy_output(plugin_name, stdout, output));
             let mut stdin = child.stdin.take().unwrap();
             io::copy(&mut input, &mut stdin)?;
         }
@@ -246,19 +294,83 @@ fn run_process<T: Read, U: Write + Send + 'static>(
     Ok(())
 }
 
-fn spawn<F, T>(f: F) -> JoinHandle<T>
+fn spawn_logger<T>(
+    tc: &ThreadCount,
+    level: Level,
+    rdr: Option<T>,
+    plugin_name: String,
+) -> JoinHandle<()>
+where
+    T: Read + Send + 'static,
+{
+    let mut bufrdr = BufReader::new(rdr.unwrap());
+    spawn(tc.clone(), move || {
+        let mut buf = String::new();
+        while bufrdr.read_line(&mut buf)? > 0 {
+            match level {
+                Level::Error => error!("PLUGIN {}: {}", plugin_name, buf.trim()),
+                Level::Warn => warn!("PLUGIN {}: {}", plugin_name, buf.trim()),
+                Level::Info => info!("PLUGIN {}: {}", plugin_name, buf.trim()),
+                Level::Debug => debug!("PLUGIN {}: {}", plugin_name, buf.trim()),
+                Level::Trace => trace!("PLUGIN {}: {}", plugin_name, buf.trim()),
+            }
+            buf.clear();
+        }
+        Ok(())
+    })
+}
+
+fn spawn<F, T>(tc: ThreadCount, f: F) -> JoinHandle<T>
 where
     F: FnOnce() -> io::Result<T>,
     F: Send + 'static,
     T: Send + 'static,
 {
+    tc.incr();
     thread::spawn(move || match f() {
-        Ok(x) => x,
+        Ok(x) => {
+            tc.decr();
+            x
+        }
         Err(x) => {
             error!("{:?}", x);
-            panic!("{:?}", x)
+            tc.decr();
+            drop(tc);
+            Err(x).unwrap()
         }
     })
+}
+
+static NEWLINE: u8 = b"\n"[0];
+
+static COLON: u8 = b":"[0];
+
+fn copy_output<T: Read, U: Write>(
+    plugin_name: String,
+    child_output: T,
+    output: U,
+) -> io::Result<()> {
+    let mut rdr = BufReader::with_capacity(1024 * 1024, child_output);
+    let mut wrt = BufWriter::with_capacity(1024 * 1024, output);
+    let mut buf = Vec::new();
+    buf.extend_from_slice(plugin_name.as_bytes());
+    buf.push(COLON);
+    while rdr.read_until(NEWLINE, &mut buf)? > 0 {
+        buf.pop();
+        if buf.ends_with(b"\r") {
+            buf.pop();
+        }
+        buf.push(NEWLINE);
+        wrt.write(&buf)?;
+        buf.truncate(plugin_name.len() + 1);
+    }
+    Ok(())
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+struct Line {
+    plugin: String,
+    output: String,
 }
 
 #[cfg(test)]
@@ -301,6 +413,8 @@ mod tests {
 
     #[test]
     fn test_run_process() {
+        //env_logger::builder().is_test(true).try_init().unwrap();
+        let tc = ThreadCount::new();
         let mut gen = Gen::new();
         let plugin = Plugin {
             name: "foo".into(),
@@ -310,15 +424,18 @@ mod tests {
             stdout: Some(true),
         };
         let proc = prep_process(&mut gen, &plugin);
-        let input_file_name = proc.input_file_name.clone().unwrap();
+        let mut expected = Vec::new();
+        expected.extend_from_slice(b"foo:");
+        expected.extend_from_slice(&proc.input_file_name.as_ref().unwrap().as_bytes());
+        expected.push(NEWLINE);
         let input = Cursor::new(b"echo $INPUT");
-        run_process(proc, input, File::create("test_run_process").unwrap()).unwrap();
-        thread::sleep(Duration::from_millis(100));
+        run_process(&tc, proc, input, File::create("test_run_process").unwrap()).unwrap();
+        tc.wait();
         let mut output = File::open("test_run_process").unwrap();
-        let mut result = [0u8; 16];
-        output.read_exact(&mut result).unwrap();
+        let mut result = Vec::new();
+        output.read_to_end(&mut result).unwrap();
         fs::remove_file("test_run_process").unwrap();
-        assert_eq!(String::from_utf8_lossy(&result), input_file_name);
+        assert_eq!(result, expected);
     }
 
     #[test]
@@ -340,10 +457,10 @@ mod tests {
         process_files(&config, files.into_iter(), open_output).unwrap();
         thread::sleep(Duration::from_millis(100));
         let mut output = File::open("test_process_files").unwrap();
-        let mut result = [0u8; 6];
-        output.read_exact(&mut result).unwrap();
+        let mut result = Vec::new();
+        output.read_to_end(&mut result).unwrap();
         fs::remove_file("test_process_files").unwrap();
-        assert_eq!(result, *b"foobar");
+        assert_eq!(&result, b"foo:foobar\n");
     }
 
     struct NamedCursor(String, Cursor<Vec<u8>>);
